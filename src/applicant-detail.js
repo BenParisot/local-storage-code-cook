678ef9172ea0d4f72447474d7c18b0c8ec8f85ed
Ben Parisot <me@benparisot.com>
2019-02-05 11:40:11 -0800

    add applicant details list to applicant-detail page, display applicant values on page

--- src/applicant-detail.js
+++ src/applicant-detail.js
@@ -1,5 +1,11 @@
 const json = window.localStorage.getItem('applicant');
 const hydrate = JSON.parse(json);
-console.log(hydrate);
 
-console.log('this works');
+const nameNode = document.getElementById('name');
+const skillNode = document.getElementById('skill');
+const foodNode = document.getElementById('fave-food');
+
+nameNode.textContent = hydrate.name;
+skillNode.textContent = hydrate.skill;
+foodNode.textContent = hydrate.faveFood;
+
