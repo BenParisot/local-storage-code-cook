262e06b6044ec18d5134a68f33f09e673419db11
Ben Parisot <me@benparisot.com>
2019-02-06 10:27:49 -0800

    get two applicants pushed into applicants array and send to localstorage

--- src/apply.js
+++ src/apply.js
@@ -1,9 +1,49 @@
-const applicant = {
-    name: 'Rudolph Ghirardelli',
+const applicant1 = {
+    firstName: 'Rudy',
+    lastName: 'Ghirardelli', 
     skill: 'flappin his gums',
-    faveFood: 'chocolate'
+    faveFood: 'unsweetened tapioca pudding'
 };
 
-const serialize = JSON.stringify(applicant);
+const newApplicant = {
+    firstName: 'Kelly',
+    lastName: 'Scampath', 
+    skill: 'public aging',
+    faveFood: 'a single jalapeno pepper once a week'
+};
 
-window.localStorage.setItem('applicant', serialize);
+
+let applicants = [];
+
+applicants.push(applicant1);
+
+// console.log(applicants);
+
+let serializeOne = JSON.stringify(applicants);
+
+window.localStorage.setItem('applicants', serializeOne);
+
+//check to see if there is already an array of applicants
+
+const jsonString = window.localStorage.getItem('applicants');
+
+
+
+
+
+//if there is, parse it and put it in the new array --  lets you preserve previous applicants
+
+if(jsonString) {
+    applicants = JSON.parse(jsonString);
+}
+
+//then serialize the new array with both existing and new objects and set to localstorage
+
+applicants.push(newApplicant);
+
+serializeOne = JSON.stringify(applicants);
+
+window.localStorage.setItem('applicants', serializeOne);
+
+
+console.log(applicants);
